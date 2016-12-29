9b0614ce0194a4f1150e8806a6626169a9762af6
JuanDevWP <blackmaxxgdl18@hotmail.com>
2016-12-29 01:09:32 -0600

    First commit

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var htmlmin = require('gulp-htmlmin');
 
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
+  sassApp: 'src/scss/app.scss',
   htmlSource : 'src/*.html',
   htmlPartialSource : 'src/partial/*.html',
   jsSource : 'src/js/**',
@@ -45,14 +46,9 @@ gulp.task('clean-images', function() {
 });
 
 gulp.task('sass', function(){
-  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
-  var sassFiles;
-  
-  
-  sassFiles = gulp.src(SOURCEPATHS.sassSource)
+  sassFiles = gulp.src(SOURCEPATHS.sassApp)
       .pipe(autoprefixer())
       .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-      return merge(bootstrapCSS, sassFiles)
           .pipe(concat('app.css'))
           .pipe(gulp.dest(APPPATH.css));
 });
@@ -64,10 +60,6 @@ gulp.task('images', ['clean-images'], function() {
       .pipe(gulp.dest(APPPATH.img));
 });
 
-gulp.task('moveFonts', function() {
-  gulp.src('./node_modules/bootstrap/dist/fonts/*.{eot,svg,ttf,woff,woff2}')
-      .pipe(gulp.dest(APPPATH.fonts));
-});
 
 gulp.task('scripts',['clean-scripts'],  function() {
   gulp.src(SOURCEPATHS.jsSource)
@@ -86,12 +78,10 @@ gulp.task('compress',  function() {
 });
 
 gulp.task('compresscss', function(){
-  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
-  var sassFiles;
+
   sassFiles = gulp.src(SOURCEPATHS.sassSource)
       .pipe(autoprefixer())
       .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-      return merge(bootstrapCSS, sassFiles)
           .pipe(concat('app.css'))
           .pipe(cssmin())
           .pipe(rename({suffix: '.min'}))
@@ -127,7 +117,7 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts', 'images', 'html'], function() {
+gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts', 'scripts', 'images', 'html'], function() {
     gulp.watch([SOURCEPATHS.sassSource], ['sass']);
     //gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
     gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
